263c89a3e555bf049f6663595473a5e0cfcb8108
Isaia Nisoli <nisoli@im.ufrj.br>
2020-05-15 15:49:23 -0300

    Initial commit

--- src/CamparyBindings.jl
+++ src/CamparyBindings.jl
@@ -1,5 +1,18 @@
 module CamparyBindings
+using StaticArrays
 
-greet() = print("Hello World!")
+export CamparyFloat, merge
+
+struct CamparyFloat{T,N}
+	val::SVector{N, T}
+	function CamparyFloat{N}(v::V) where {T, N, V<:AbstractArray{T,1}}
+		@assert length(v)==N
+		new{T, N}(SVector{N, T}(v))
+	end
+end
+
+merge(x::CamparyFloat{T,K}, y::CamparyFloat{T,L}) where {T,K,L} = CamparyFloat{T, K+L}([x.val ; y.val]) 
+
+
 
 end # module
